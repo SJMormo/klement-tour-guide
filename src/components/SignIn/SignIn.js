ff361a0094362624ef4ce537ba751bbf1ab9ef4c
SJMormo <sjm91097@gmail.com>
2022-04-18 01:56:29 +0600

    Google sign up added

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,58 +1,52 @@
 import React from 'react';
-import { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { useNavigate } from 'react-router-dom';
-import auth from '../../firebase.init';
-import Loading from '../Loading/Loading';
+import { Link } from 'react-router-dom';
+import googleIcon from '../../images/google-logo.png';
+
 
 const SignIn = () => {
-    const navigate = useNavigate();
-    const emailRef = useRef('');
-    const passwordRef = useRef('');
-    const [
-        createUserWithEmailAndPassword,
-        user,
-        loading,
-        error,
-    ] = useCreateUserWithEmailAndPassword(auth);
 
-    const handleSignIn = e => {
-        e.preventDefault();
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
-
-        console.log(email, password);
-        createUserWithEmailAndPassword(email, password);
-    }
-
-    if (loading) {
-        return <Loading></Loading>
-    }
-
-    // after login redirect to home
-    if (user) {
-        navigate('/home');
-    }
 
     return (
         <div>
             <h3 className='text-center mt-5'>Sign In Here</h3>
-            <Form onSubmit={handleSignIn} className='w-50 mx-auto'>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
-                </Form.Group>
+            <div className='w-50 mx-auto'>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
-                </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
 
-                <Button variant="primary" type="submit">
-                    Sign In
-                </Button>
-            </Form>
+                    <Button className='w-50 d-block mx-auto' variant="primary" type="submit">
+                        Sign Up
+                    </Button>
+                </Form>
+            </div>
+
+            <div className='mt-3 d-flex justify-content-center'>
+                <div className='w-25 px-4'>
+                    <hr />
+                </div>
+                <p>or</p>
+                <div className='w-25 px-4'>
+                    <hr />
+                </div>
+            </div>
+            <Button className='w-25 d-block mx-auto' variant="light" type="submit">
+                <img src={googleIcon} alt="" />
+                <span className='ms-2'>Continue With Google</span>
+            </Button>
+            <div>
+                <hr className='w-50 mx-auto mt-5' />
+                <p className='text-center'>
+                    Don't Have an Account? <Link className="form-link" to="/signup">Create an account</Link>
+                </p>
+            </div>
         </div>
     );
 };
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,86 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import Loading from '../Loading/Loading';
+import googleIcon from '../../images/google-logo.png';
 
 const SignUp = () => {
+    const navigate = useNavigate();
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    // user sign up
+    const handleSignUp = e => {
+        e.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        console.log(email, password);
+        createUserWithEmailAndPassword(email, password);
+    }
+
+    // sign up with google
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+
+    if (loading || googleLoading) {
+        return <Loading></Loading>
+    }
+
+    // after login redirect to home
+    if (user || googleUser) {
+        navigate('/home');
+    }
+
     return (
         <div>
-            <h3>Sign Up</h3>
+            <h3 className='text-center mt-5'>Sign Up Here</h3>
+            <Form onSubmit={handleSignUp} className='w-50 mx-auto'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Button className='w-50 d-block mx-auto' variant="primary" type="submit">
+                    Sign Up
+                </Button>
+            </Form>
+
+
+            <div className='mt-3 d-flex justify-content-center'>
+                <div className='w-25 px-4'>
+                    <hr />
+                </div>
+                <p>or</p>
+                <div className='w-25 px-4'>
+                    <hr />
+                </div>
+            </div>
+
+
+            <Button onClick={() => signInWithGoogle()} className='w-25 d-block mx-auto' variant="light" type="submit">
+                <img src={googleIcon} alt="" />
+                <span className='ms-2'>Sign Up With Google</span>
+            </Button>
+
+            <div>
+                <hr className='w-50 mx-auto mt-5' />
+                <p className='text-center'>
+                    Already Have an Account? <Link className="form-link" to="/signin">Sign In</Link>
+                </p>
+            </div>
         </div>
     );
 };
