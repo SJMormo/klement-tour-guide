984ad931214b4d390a06c24c5088c33cbd8f86dd
SJMormo <sjm91097@gmail.com>
2022-04-18 02:17:58 +0600

    Website title changed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h3>Home</h3>
+            <h3>Klement's Bandarban Tour Guide</h3>
         </div>
     );
 };
