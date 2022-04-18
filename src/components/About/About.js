33ba63e6ccfec0c2cdc74ce18a1230ac30028684
SJMormo <sjm91097@gmail.com>
2022-04-18 19:57:55 +0600

    About me section added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h3>About</h3>
+        <div className='mt-5 w-50 mx-auto shadow rounded-3'>
+            <h3 className='text-center p-2'>About me!</h3>
+            <p className='mt-3 text-center p-2'>
+                My Name's Mormo. Currently I am pursuing my BSc. in CSE, RUET. My goal in life is to become a full stack web developer. In order to reach to my goal, my strategy is to take small steps everyday. My target is to work for my goal every single day. To achieve that small step, I try to keep myself motivated to follow a daily routine.
+            </p>
         </div>
     );
 };
